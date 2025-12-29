6d7d78f812749734a5af9495ccf68cbd630029be
pawan mishra <pawanm9816@gmail.com>
2025-12-29 21:08:44 +0545

    Day 5

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = models.AutoField() // It is created automatically
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    email = models.EmailField(null= True , blank= True)
+    address = models.TextField(null= True , blank= True)
+    # file = models.FileField()
+
+    class Product(models.Model):
+        pass
