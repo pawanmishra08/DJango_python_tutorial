767a3f0c4b715d132733ab41e5cb6a3288b88586
pawan mishra <pawanm9816@gmail.com>
2026-01-15 10:10:00 +0545

    Day 9 , update and search added

--- vege/views.py
+++ vege/views.py
@@ -22,11 +22,37 @@ def receipes(request):
         return redirect('/receipes/') #Redirect to the same page after POST request
 
     queryset = Receipe.objects.all()
-    context = {'receipes': queryset}
 
-        # print(receipe_name, receipe_description, receipe_image)
-        # print(data)
+    if request.GET.get('search'):
+        queryset = queryset.filter(receipe_name__icontains=request.GET.get('search'))
+
+
+    context = {'receipes': queryset}
     return render(request, "receipes.html", context)
+
+def update_receipe(request, id):
+    queryset = Receipe.objects.get(id = id)
+
+    if request.method =="POST":
+        data = request.POST
+
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+        receipe_image = request.FILES.get('receipe_image')
+
+        queryset.receipe_name = receipe_name
+        queryset.receipe_description = receipe_description
+
+        if receipe_image:
+            queryset.receipe_image = receipe_image
+
+        queryset.save()
+
+        return redirect('/receipes/') #Redirect to the same page after POST request
+
+
+    context = {'receipe': queryset }
+    return render(request, "update_receipe.html",context)
 
 def delete_receipe(request, id):
    queryset = Receipe.objects.get(id = id)
