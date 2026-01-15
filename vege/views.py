5219912a1ae1f8b937df0a32a757c5a94d054701
pawan mishra <pawanm9816@gmail.com>
2026-01-15 22:41:30 +0545

    Day11, register functionalities added git add .

--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Receipe
 
 # Create your views here.
@@ -58,3 +59,33 @@ def delete_receipe(request, id):
    queryset = Receipe.objects.get(id = id)
    queryset.delete()
    return redirect('/receipes/')
+
+def login_page(request):
+    return render(request, "login.html")
+
+def register_page(request):
+
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.info(request, "Username already taken !!!")
+            return redirect('/register/')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+            )
+        user.set_password(password) # to hash/encrypt the password
+        user.save()
+
+        messages.info(request, "Account created successfully !!!")
+
+
+        return redirect('/register/')
+    return render(request, "register.html")
