899e68d9fdbf63be2894d72629284956c11dd166
pawan mishra <pawanm9816@gmail.com>
2026-01-26 21:14:32 +0545

    Day21, Custom User Model git add .!

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    username = None
+    phone_number = models.CharField(max_length= 100, unique = True)
+    email = models.EmailField(unique= True)
+    user_bio = models.CharField(max_length= 50)
+    user_profile = models.ImageField(upload_to= "profile")
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager()
--- vege/views.py
+++ vege/views.py
@@ -6,13 +6,16 @@ from .models import *
 from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your views here.
 
 @login_required(login_url='/login/')
 def receipes(request):
 
-    if not request.user.is_authenticated:
-        return HttpResponse("user is not authenticated")
+    # if not request.user.is_authenticated:
+    #     return HttpResponse("user is not authenticated")
     if request.method == "POST":
         data = request.POST
 
@@ -73,10 +76,6 @@ def login_page(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        # Debugging: See what is actually coming from the browser
-        # print(f"DEBUG: Username entered: '{username}'")
-        # print(f"DEBUG: Password entered: '{password}'")
 
         if not User.objects.filter(username = username).exists():
             messages.error(request, "Username already exists !!!")
